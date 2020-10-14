547106dd618642ac0bf5ee963f7e1d09e3b8adc0
larry <larrymayster@gmail.com>
2020-10-14 15:57:03 +0900

    Writes to a socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("localhost:8080")?;
+
+    stream.write_all(b"Hello, World!")?;
     println!("Hello, world!");
+    Ok(())
 }
