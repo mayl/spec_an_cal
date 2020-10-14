19bb92f14994951c8b6a0c763486ab83e5668731
larry <larrymayster@gmail.com>
2020-10-14 16:49:42 +0900

    Receive bytes over the socket

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use std::net::TcpStream;
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("localhost:8080")?;
 
-    stream.write_all(b"Hello, World!")?;
-    println!("Hello, world!");
+    stream.write_all(b"Hello, World!")?; 
+    let mut buf = [0; 10];
+    let _n = stream.read(&mut buf[..]);
+    let rx_val = String::from_utf8_lossy(&buf);
+    println!("Received the bytes: {:?}", rx_val);
     Ok(())
 }
