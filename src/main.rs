53659f377f1b3852a0e7dc7fd50226035d550acd
larry <larrymayster@gmail.com>
2020-10-20 16:16:40 +0900

    Start adding TUI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
 use std::net::TcpStream;
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("localhost:8080")?;
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block,chunks[0]);
+        let block = Block::default()
+            .title("Block 2")
+            .borders(Borders::ALL);
+        f.render_widget(block,chunks[1]);
+        let block = Block::default()
+            .title("Block 3")
+            .borders(Borders::ALL);
+        f.render_widget(block,chunks[2]);
+    })?;
 
+    let mut stream = TcpStream::connect("localhost:8080")?;
     stream.write_all(b"Hello, World!")?; 
     let mut buf = [0; 10];
     let _n = stream.read(&mut buf[..]);
