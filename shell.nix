2ad5c5dd2ffa2c3d96d9442603c51a6dd9fd1ca2
larry <larrymayster@gmail.com>
2020-10-23 15:30:27 +0900

    mingw gcc is in the path, pthreads missing
    This successfully builds a shell with a mingw compiler, but the cargo
    build now fails because it isn't able to link against pthreads.  This
    seems like somewhat of a known (maybe solved) issue, but I can't figure
    out how to incorporate the solution (yet).  Using the "buildPackages"
    prefix also removes the requirement for `{allowUnsupporteSystem =
    true;}`

--- shell.nix
+++ shell.nix
@@ -2,7 +2,12 @@ let
   sources = import ./nix/sources.nix;
   rust = import ./nix/rust.nix{inherit sources;};
   pkgs = import sources.nixpkgs{};
+  mingw = (import <nixpkgs>{ crossSystem = { config = "x86_64-pc-mingw32"; libc = "msvcrt"; crossThreads = true; }; }).buildPackages.gcc;
 in
 pkgs.mkShell {
-    buildInputs = [ rust pkgs.pkgsCross.mingwW64.gcc ];
+    buildInputs = [ 
+	rust 
+	#mingw
+	pkgs.pkgsCross.mingwW64.buildPackages.gcc
+    ];
 }
